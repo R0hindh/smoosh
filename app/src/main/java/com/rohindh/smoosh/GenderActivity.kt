d45631b964092732f5a75d7d16cee79bed49005c
rohindh <rohindhrock2411@gmail.com>
2019-06-23 22:57:30 +0530

    lesson 2

---
+++ app/src/main/java/com/rohindh/smoosh/GenderActivity.kt
@@ -0,0 +1,12 @@
+package com.rohindh.smoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GenderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gender)
+    }
+}
